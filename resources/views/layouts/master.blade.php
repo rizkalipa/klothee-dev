aa957a927a53da7b29f3d9a012f82de5dcf12ae5
Rizky Aji Rahmansyah <rizkyajirahmansyah@gmail.com>
2019-08-12 17:23:55 +0700

    Mengganti shadow hover pada button

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,7 +26,7 @@
                         {{ Auth::user()->name }} <span class="caret"></span>
                     </a>
 
-                    <div class="dropdown-menu dropdown-menu-right border-0 shadow" aria-labelledby="navbarDropdown">
+                    <div class="dropdown-menu dropdown-menu-right border-0 shadow text-center" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('logout') }}"
                            onclick="event.preventDefault();
                                          document.getElementById('logout-form').submit();">
