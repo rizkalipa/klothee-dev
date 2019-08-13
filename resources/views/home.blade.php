7a0cde69b35ea8ef9a59c7315371802db342b61e
Rizky Aji Rahmansyah <rizkyajirahmansyah@gmail.com>
2019-08-13 14:56:41 +0700

    Merubah desain interface home page, menambah class overlay, dan memperbaiki css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.master')
 
-@section('Home Page')
+@section('title', 'Home Page')
 
 @section('content')
-<div class="container h-100">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+<div class="container">
+    <!-- Jumbotron -->
+    <div class="card card-image border-0 shadow rounded-lg" style="background-image: url(https://mdbootstrap.com/img/Photos/Others/forest2.jpg);
+        background-size: 100%;">
+        <div class="text-white text-center py-5 px-4" style="position: relative; z-index: 2">
+            <div class="py-5">
+                <!-- Content -->
+                <h5 class="h5 orange-text"><i class="fas fa-camera-retro"></i> Photography</h5>
+                <h2 class="card-title h2 my-4 py-2">Jumbotron with image overlay</h2>
+                <p class="mb-4 pb-2 px-md-5 mx-md-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur obcaecati vero aliquid libero doloribus ad, unde tempora maiores, ullam, modi qui quidem minima debitis perferendis vitae cumque et quo impedit.</p>
+                <a class="btn peach-gradient"><i class="fas fa-clone left"></i> View project</a>
             </div>
         </div>
+        <div class="rounded-lg overlay"></div>
     </div>
+    <!-- Jumbotron -->
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('Welcome !')
+@section('title', 'Welcome !')
 
 @section('content')
 <div class="container">
@@ -12,8 +12,8 @@
                 <p class="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
                     optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
                 </p>
-                <a class="btn btn-primary" href="{{ route('login') }}"><h5 class="m-0"><i class="fas fa-check-circle mr-2"></i> 
-                    Login Now!</h5></a>
+                <a class="btn btn-primary" href="{{ route('register') }}"><h5 class="m-0"><i class="fas fa-check-circle mr-2"></i> 
+                    Register Now!</h5></a>
             </div>
         </div>
     </div>
