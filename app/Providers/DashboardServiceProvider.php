2212943301e5fd1917760fa324556ed64fa95a3c
Rizky Aji Rahmansyah <rizkyajirahmansyah@gmail.com>
2019-08-27 11:52:41 +0700

    Membuat logic calendar untuk menentukan waktu saat ini dan membuat class pada hari yang terpilih (hari ini)

---
+++ app/Providers/DashboardServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Carbon\Carbon;
+use \App\Post;
+
+class DashboardServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('posts.index', function($view)
+        {
+            $user = auth()->user();
+
+            $postPublished = \App\Post::where('user_id', $user->id)->where('status', 'Publish')->get();
+            $postDraft = \App\Post::where('user_id', $user->id)->where('status', 'Draft')->get();
+
+            return $view->with(['postPublished' => $postPublished, 'postDraft' => $postDraft]);
+        });
+
+        View::composer('components.calendar', function($view)
+        {
+            $thisMonth = new Carbon;
+
+            switch( $thisMonth->startOfMonth()->format('D') )
+            {
+                case('Mon'): $firstMonthSpan = 0;
+                            break;
+                case('Tue'): $firstMonthSpan = 1;
+                            break;
+                case('Wed'): $firstMonthSpan = 2;
+                            break;
+                case('Thu'): $firstMonthSpan = 3;
+                            break;
+                case('Fri'): $firstMonthSpan = 4;
+                            break;
+                case('Sat'): $firstMonthSpan = 5;
+                            break;
+                case('Sun'): $firstMonthSpan = 6;
+                            break;
+            }
+
+            return $view->with(['firstDaySpan' => $firstMonthSpan, 'thisMonth' => $thisMonth]);
+        });
+    }
+}
--- resources/views/components/calendar.blade.php
+++ resources/views/components/calendar.blade.php
@@ -1,5 +1,17 @@
 <div class="calendar-container">
-    @for($i = 0; $i < now()->daysInMonth; $i++)
-        <p class="calendar-date">{{ $i + 1 }}</p>
+    <p>Mon</p>
+    <p>Tue</p>
+    <p>Wed</p>
+    <p>Thu</p>
+    <p>Fri</p>
+    <p>Sat</p>
+    <p>Sun</p>
+
+    @for($i = 0; $i < $firstDaySpan; $i++)
+        <p ></p>
+    @endfor
+
+    @for($i = 0; $i < $thisMonth->daysInMonth; $i++)
+        <p class="{{ $thisMonth->now()->day == ($i + 1) ? 'selected-date' : 'calendar-date' }}">{{ $i + 1 }}</p>
     @endfor
 </div>
